3147090e2456f8c3d3d55eec2f24648ed52fd6b7
Chrystian <chrystianfarias@hotmail.com>
2023-03-19 14:05:02 -0400

    listagem de exames

--- src/main/java/com/example/avaliacao/controller/ExamesController.java
+++ src/main/java/com/example/avaliacao/controller/ExamesController.java
@@ -1,7 +1,10 @@
 package com.example.avaliacao.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +22,14 @@ public class ExamesController {
 	
 	
 	@GetMapping("/home")
-	public String pagina() {		
+	public String pagina(Model model) {
+		List <Exame> exames = exameRepository.findAll();
+		model.addAttribute("exames",exames);
 		return "examesprincipal";
+		
+		
+		
+		
 	}
 	
 	

ed839b85cf73cdc1adddc0475f3b485a306f793e
Chrystian <chrystianfarias@hotmail.com>
2023-03-20 16:39:29 -0400

    atualizando exames

--- src/main/java/com/example/avaliacao/controller/ExamesController.java
+++ src/main/java/com/example/avaliacao/controller/ExamesController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -56,6 +57,12 @@ public class ExamesController {
 		return "formteste";
 		
     }
+	
+	@PostMapping("/salvar")
+	public String salvarExame(@ModelAttribute("exame") Exame exame) {
+		exameRepository.save(exame);
+		return "teste";
+	}
 	
 	
 	
